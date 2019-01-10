75cf285aa63fc5312002374521fb1a7d6468c8cc
Anmol Jande <jandanmol@gmail.com>
2019-01-10 13:17:16 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,47 @@
+var chocolateBars=["snickers","hundered grand","kitkat","skittles"];
+
+function addElementToBeginningOfArray(arr,element)
+{
+  narr=[element,...arr];
+  return narr;
+  
+}
+
+function destructivelyAddElementToBeginningOfArray(arr,element)
+{
+  arr.unshift(element);
+  return arr;
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array)
+{
+  array.shift();
+  return array;
+}
+function accessElementArray(arr,index)
+{
+  return arr[index];
+}
+function addElementToEndOfArray(array, element)
+{
+  array.push(element);
+  return array;
+}
+
+function removeElementFromBeginningOfArra(arra)
+{
+  return arra.slice(0,1);
+}
+
+function destructivelyRemoveElementFromEndOfArray(arra)
+{
+  arra.pop();
+  return arra;
+}
+
+
+function removeElementFromEndOfArray (array)
+{
+  
+  return array.slice(0,array.length-1)
+}
